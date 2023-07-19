3ed5ce91ef66c0486d7b4bfabb0104cc17251037
OrTab <or.1532.tabin@gmail.com>
2023-07-20 00:27:28 +0300

    Add call implementation with custom values

--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ import time
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 import json
-from config import MY_EMAIL 
+from config import MY_EMAIL, TWILIO_ACCOUNT_ID, TWILIO_AUTH_TOKEN, MY_NUMBER, MY_TWILIO_NUMBER, CUSTOM_MESSAGE, REPEAT_MESSAGE_VALUE
+from twilio.rest import Client
+
 
 CREDENTIALS_FILE_PATH = '../credentials.json'
 CACHED_EVENTS_FILE_PATH = "events.json"
@@ -12,12 +14,20 @@ CACHED_EVENTS_FILE_PATH = "events.json"
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 MAX_EVENTS_RESULTS = 10
 LAST_FETCH_TIME = None
+FETCH_PERIOD = 10 * 60 * 1000
 
 TIME_TO_CALL_BEFORE_EVENT = {
     "value": 1,
     "unit":"minutes"
 }
 
+def call(event):
+    account_sid = TWILIO_ACCOUNT_ID
+    auth_token = TWILIO_AUTH_TOKEN
+    client = Client(account_sid, auth_token)
+    custom_message = CUSTOM_MESSAGE.format(event["summary"])
+    twiml = f'<Response><Say loop="{REPEAT_MESSAGE_VALUE}">{custom_message}</Say></Response>'
+    call = client.calls.create(twiml=twiml,to= MY_NUMBER,from_=MY_TWILIO_NUMBER)
 
 def get_time_difference_per_unit(time_diff_sec):
     unit = TIME_TO_CALL_BEFORE_EVENT['unit']
@@ -31,8 +41,8 @@ def get_time_difference_per_unit(time_diff_sec):
 
 def get_calendar_events(user_email):
     global LAST_FETCH_TIME
-    should_fetch = (LAST_FETCH_TIME is None or time.time() - LAST_FETCH_TIME > 10 * 60 * 1000)
-    print('should_fetch',should_fetch)
+    should_fetch = (LAST_FETCH_TIME is None or time.time() - LAST_FETCH_TIME > FETCH_PERIOD)
+    print('should_fetch: ',should_fetch)
     events_result = None
     if not should_fetch and os.path.exists(CACHED_EVENTS_FILE_PATH):
         with open(CACHED_EVENTS_FILE_PATH, "r") as file:
@@ -74,8 +84,8 @@ def get_calendar_events(user_email):
                 current_datetime_timestamp = datetime.datetime.utcnow().timestamp()
                 time_difference_in_seconds = (event_timestamp - current_datetime_timestamp)
                 time_difference = get_time_difference_per_unit(time_difference_in_seconds)
-                print(time_difference)
                 if (time_difference <= 1 and time_difference > 0):
+                    call(event)
                     return
 
     
