cf8fc5cca23b04b368168528dcf86038a73e99e8
OrTab <or.1532.tabin@gmail.com>
2023-07-20 01:31:20 +0300

    removed hours and days support for now , will implement in the future

--- src/main.py
+++ src/main.py
@@ -21,10 +21,12 @@ from config import (
 from twilio.rest import Client
 
 LAST_FETCH_TIME = None
-TIME_TO_CALL_BEFORE_EVENT = {"value": 1, "unit": "minutes"}
+TIME_TO_CALL_BEFORE_EVENT = {"value": 12, "unit": "minutes"}
 
 
 def call(event):
+    print(event)
+    return
     account_sid = TWILIO_ACCOUNT_ID
     auth_token = TWILIO_AUTH_TOKEN
     client = Client(account_sid, auth_token)
@@ -35,17 +37,16 @@ def call(event):
 
 def get_time_difference_per_unit(time_diff_sec):
     unit = TIME_TO_CALL_BEFORE_EVENT["unit"]
-    return {
+    diff = {
         "seconds": time_diff_sec,
         "minutes": time_diff_sec / 60,
-        "hours": time_diff_sec / 60 / 60,
-        "days": time_diff_sec / 60 / 60 / 24,
     }[unit]
+    return diff
 
 
 def get_calendar_events(user_email):
     global LAST_FETCH_TIME
-    should_fetch = (
+    should_fetch = True and (
         LAST_FETCH_TIME is None or time.time() - LAST_FETCH_TIME > FETCH_PERIOD
     )
     print("should_fetch: ", should_fetch)
@@ -93,7 +94,11 @@ def get_calendar_events(user_email):
             current_datetime_timestamp = datetime.datetime.utcnow().timestamp()
             time_difference_in_seconds = event_timestamp - current_datetime_timestamp
             time_difference = get_time_difference_per_unit(time_difference_in_seconds)
-            if time_difference <= 1 and time_difference > 0:
+            print(time_difference)
+            if (
+                time_difference <= TIME_TO_CALL_BEFORE_EVENT["value"]
+                and time_difference > 0
+            ):
                 call(event)
                 return
 
